0d92bf3e6b301e444638dadec5fb9df1d784f201
dehmer <horst.dehmer@syncpoint.io>
2024-11-28 12:24:56 +0100

    ID3v1.1 support.

--- bin/mmread.js
+++ bin/mmread.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
+import fs from 'node:fs'
 import { open } from 'node:fs/promises'
 import { dirname, basename, extname } from 'node:path'
+import { randomUUID } from 'node:crypto'
 import * as R from 'ramda'
 import minimist from 'minimist'
 import { glob, globSync, globStream, globStreamSync, Glob } from 'glob'
+import { PromisePool } from '@supercharge/promise-pool'
 import * as id3v1 from '../lib/id3v1.js'
 import * as id3v2 from '../lib/id3v2.js'
 
@@ -11,43 +14,64 @@ const ROOT = '/Volumes/audiothek'
 // const ROOT = '/Users/dehmer/Public/Data/audio'
 const EXTENSIONS = ['aac', 'aif', 'aiff', 'flac', 'm4a', 'm4v', 'mp3', 'mpc', 'ogg', 'wav', 'wma']
 // const PATTERN = `${ROOT}/**/*.{${EXTENSIONS.join(',')}}`
-const PATTERN = `${ROOT}/**/*.mp3`
+// const PATTERN = `${ROOT}/**/*.mp3`
+const PATTERN = `${ROOT}/**/02_20 - Keine Sterne in Athen.mp3`
 
 const stat = async filehandle => {
-  return filehandle.stat()
-  // const stat = await filehandle.stat()
-  // return Object.entries(stat).reduce((acc, [key, value]) => {
-  //   acc['stat/' + key] = value
-  //   return acc
-  // }, {})
+  const { atime, mtime, ctime, size } = await filehandle.stat()
+  return { atime, mtime, ctime, size }
 }
 
 const path = filename => ({
+  filename,
   dirname: dirname(filename),
-  extname: extname(filename),
+  extname: extname(filename).substring(1),
   basename: basename(filename)
 })
 
+const writeImage = context => {
+  // Write first image to file
+  const apic = context[`${context.id}/APIC`] || context[`${context.id}/PIC`]
+  if (apic) {
+    const image = apic[0]
+    console.log('image data', !!image)
+    const ext = image.mimetype
+      ? image.mimetype.substring(image.mimetype.indexOf('/') + 1)
+      : image.format.toLowerCase()
+    fs.writeFileSync(`/Users/dehmer/Downloads/tmp/${randomUUID()}.${ext}`, image.data)
+  } else console.log('image data', false)  
+}
+
+const readmeta = async filename => {
+  const filehandle = await open(filename, 'r')    
+  const context = {
+    uuid: randomUUID(),
+    ...path(filename),
+    ...await stat(filehandle),
+  }
+
+  Object.assign(context, {
+    ...await id3v1.read(filehandle, context),
+    ...await id3v2.read(filehandle, context)
+  })
+
+  await filehandle.close()
+  return context
+}
+
 ;(async () => {
   const filenames = await glob(PATTERN, { nodir: true })
-  const xs = await filenames.reduce(async (acc, filename) => {
-    const xs = await acc
-    const filehandle = await open(filename, 'r')
-    const info = {
-      ...path(filename),
-      ...await stat(filehandle),      
-    }
-
-    Object.assign(info, await id3v2.read(filehandle, info))
-    if (info.version) xs.push(info.version)
-
-    await filehandle.close()
-    return xs
+  
+  PromisePool
+    .withConcurrency(10)
+    .for(filenames)
+    .process(async filename => {
+      // console.log('reading', filename)
+      const context = await readmeta(filename)
+      console.log(context)
+    })
   
     // const info = await id3v1.read(filehandle, stat)
     // if (Object.keys(info).length !== 0) console.log(filename, Object.keys(info).length)
     // await filehandle.close()
-  }, [])
-
-  console.log(R.uniq(xs))
 })()
